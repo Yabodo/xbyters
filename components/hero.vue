a3ec070866197bc830537f4cf2a1ec73db7681e5
Yabodo <yabood@vivaldi.net>
2023-07-29 11:46:18 +0300

    Animate hero

--- components/hero.vue
+++ components/hero.vue
@@ -1,36 +1,30 @@
 <template>
   <div
-    class="relative flex flex-col-reverse px-4 py-16 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full"
+    class="relative flex flex-col-reverse px-4 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md:px-8 sm:max-w-xl md:max-w-full"
   >
     <div
-      class="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center"
+      class="hidden lg:block z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center"
     >
-      <img
-        src="/laptop.png"
-        class="animate__animated animate__slideInLeft object-cover object-right w-full h-auto lg:w-auto lg:h-full"
-        alt=""
-      />
+      <div id="lottie-animation"></div>
     </div>
     <div
-      class="relative flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl"
+      class="my-16 lg:my-0 relative flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl"
     >
       <div class="mb-16 lg:pr-5 lg:max-w-lg lg:mb-0">
         <div class="animate__animated animate__slideInRight max-w-xl mb-6">
           <h2
-            class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-none"
+            class="text-center md:text-left max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-none"
           >
-            We will bring your<br class="hidden md:block" />
-            business
-            <span class="inline-block text-deep-purple-accent-400">online</span>
+            We will bring your business online so you don't have to.
           </h2>
-          <p class="">
+          <p class="text-center md:text-left">
             We stay up-to-date with the latest trends and technologies in web
             development. Our team leverages cutting-edge tools, frameworks, and
             methodologies to deliver innovative and future-proof solutions. By
             working with us, you can benefit from the latest advancements in the
-            industry.
+            industry. Take your website off your plate!
           </p>
-          <div class="mt-8">
+          <div class="mt-8 flex justify-center md:justify-start">
             <NuxtLink
               to="/contact"
               type="submit"
@@ -44,3 +38,24 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import lottie from "lottie-web";
+//import "@lottiefiles/lottie-interactivity";
+
+export default defineComponent({
+  name: "YourComponent",
+  setup() {
+    onMounted(() => {
+      lottie.loadAnimation({
+        container: document.getElementById("lottie-animation")!, // the DOM element that will contain the animation
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "/unicycle.json", // the path to the animation json
+      });
+    });
+  },
+});
+</script>
