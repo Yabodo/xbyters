69d09aac443abd1cc8f25d2939076409d1ca1de6
Yabodo <yabood@vivaldi.net>
2023-08-28 20:58:05 +0300

    Move calendar separate from contact

---
+++ pages/calendar/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Calendly inline widget begin -->
+  <div
+    class="calendly-inline-widget"
+    data-url="https://calendly.com/xbyters/30min?hide_landing_page_details=1&hide_gdpr_banner=1&background_color=0b172a&text_color=ffffff"
+    style="min-width: 320px; height: 901px"
+  ></div>
+  <!-- Calendly inline widget end -->
+</template>
+<script setup lang="ts">
+useHead({
+  title: "Contact - xbyters",
+  script: [{ src: "https://assets.calendly.com/assets/external/widget.js" }],
+});
+</script>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,15 +1,83 @@
 <template>
-  <!-- Calendly inline widget begin -->
-  <div
-    class="calendly-inline-widget"
-    data-url="https://calendly.com/xbyters/30min?hide_landing_page_details=1&hide_gdpr_banner=1&background_color=0b172a&text_color=ffffff"
-    style="min-width: 320px; height: 901px"
-  ></div>
-  <!-- Calendly inline widget end -->
+  <section>
+    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2
+        class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+      >
+        Contact Us
+      </h2>
+      <p
+        class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl"
+      >
+        Contact us to get a FREE 30 minutes consultancy session!
+      </p>
+      <form
+        action="https://formspree.io/f/mzbaykll"
+        method="POST"
+        class="space-y-8"
+      >
+        <div>
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >* Your email</label
+          >
+          <input
+            type="email"
+            id="email"
+            name="email"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="Email address"
+            required
+          />
+        </div>
+        <div>
+          <label
+            for="name"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >* Your name</label
+          >
+          <input
+            type="text"
+            id="name"
+            name="name"
+            class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="Full Name"
+            required
+          />
+        </div>
+        <div class="sm:col-span-2">
+          <label
+            for="message"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >Your message</label
+          >
+          <textarea
+            id="message"
+            rows="6"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="..."
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800"
+        >
+          Send message
+        </button>
+      </form>
+    </div>
+  </section>
 </template>
 <script setup lang="ts">
 useHead({
   title: "Contact - xbyters",
-  script: [{ src: "https://assets.calendly.com/assets/external/widget.js" }],
+  meta: [
+    {
+      name: "description",
+      content:
+        "Get in touch with us to discuss your website dreams and needs. We are easy to chat with!",
+    },
+  ],
 });
 </script>
