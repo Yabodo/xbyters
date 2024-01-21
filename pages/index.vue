c266834ec8e9cae6db25e76da23f69f9d71c36ef
Yabodo <yabood@vivaldi.net>
2024-01-21 15:30:09 +0200

    Index background upgrade

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,13 @@
     <IndexBanner class="my-10 mt-20" />
     <IndexWpDesign class="my-10 mt-20" />
     <IndexWpManagement class="my-10" />
-    <IndexFounder class="my-10" />
+    <div class="relative">
+      <img
+        src="/home2.webp"
+        class="hidden dark:block absolute right-0 top-0 -z-10 2xl:w-1/2"
+      />
+      <IndexFounder class="my-10" />
+    </div>
     <IndexCompanies class="my-10" />
     <IndexTestimonials class="my-10" />
     <IndexCta class="my-10 pt-5 h-96 flex flex-col justify-center" />
